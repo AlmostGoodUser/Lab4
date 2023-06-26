2a70e170af115875535269ecadcf763b117ad954
AlmostGoodUser <dsw49545@student.dsw.edu.pl>
2023-06-26 20:02:37 +0200

    --log default added

--- skrypt.sh
+++ skrypt.sh
@@ -11,3 +11,14 @@ then
 	echo "--logs 'value' -tworzy value ilość dokumentów tekstowych z danymi wewnątrz"
 	echo "--help -wyświetla dostępne flagi"
 fi
+
+if [ "$1" == "--logs" ]
+then
+	i=1
+	while [ $i -le 100 ]
+	do
+		touch log$i.txt
+		echo log$i.txt $0 $(date) >> log$i.txt
+		i=$(($i+1))
+	done
+fi
