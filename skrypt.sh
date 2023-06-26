16cc38bc8a4f93e621bb0705b734bc969601cd2b
AlmostGoodUser <dsw49545@student.dsw.edu.pl>
2023-06-26 19:23:45 +0200

    flag help added to skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -3,3 +3,11 @@ if [ "$1" == "--date" ]
 then
 	echo $(date)
 fi
+
+if [ $1 == "--help" ]
+then
+	echo "--date -podaje aktualną date"
+	echo "--logs -tworzy 100 dokumentów tekstowych z danymi wewnątrz"
+	echo "--logs 'value' -tworzy value ilość dokumentów tekstowych z danymi wewnątrz"
+	echo "--help -wyświetla dostępne flagi"
+fi
