76a077c1eae481c7a767ed898c19dd6f32706d49
AlmostGoodUser <dsw49545@student.dsw.edu.pl>
2023-06-26 20:24:48 +0200

    New flags added

--- skrypt.sh
+++ skrypt.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-if [ "$1" == "--date" ]
+if [ "$1" == "--date" ] || [  "$1" == "-d" ]
 then
 	echo $(date)
 fi
 
-if [ $1 == "--help" ]
+if [ $1 == "--help" ] || [  "$1" == "-h" ]
 then
 	echo "--date -podaje aktualną date"
 	echo "--logs -tworzy 100 dokumentów tekstowych z danymi wewnątrz"
 	echo "--logs 'value' -tworzy value ilość dokumentów tekstowych z danymi wewnątrz"
 	echo "--help -wyświetla dostępne flagi"
+	echo "-d -podaje aktualną date"
+	echo "-l -tworzy 100 dokumentów tekstowych z danymi wewnątrz"
+	echo "l 'value' -tworzy value ilość dokumentów tekstowych z danymi wewnątrz"
+	echo "-h -wyświetla dostępne flagi"
 fi
 
-if [ "$1" == "--logs" ]
+if [ "$1" == "--logs" ] || [  "$1" == "-l" ]
 then
 	i=1
 	while [ $i -le 100 ]
@@ -25,7 +29,7 @@ fi
 
 if [ $2 ]
 then
-	if [ $1 == "--logs" ]
+	if [ $1 == "--logs" ] || [  "$1" == "-l" ]
 	then
 	i=1
 	while [ $i -le $2 ]
